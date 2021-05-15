2b48eba58e500fe2147046d952210d1be454efc7
TranNguyen01 <67365431+TranNguyen01@users.noreply.github.com>
2021-05-15 11:53:24 +0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,22 @@
+{
+    "Food": [
+        {"id":1, "category":1,"name":"Pizza","img":"https://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg", "price":500000, "moreInfo": "Nhiều calory"},
+        {"id":2, "category":1,"name":"Pizza","img":"https://www.helpguide.org/wp-content/uploads/table-with-grains-vegetables-fruit-768.jpg", "price":500000, "moreInfo": "Nhiều calory"},
+        {"id":3, "category":3,"name":"Pizza","img":"https://www.efsa.europa.eu/sites/default/files/styles/share_opengraph/public/2021-03/experts_food_additives.jpg?h=827069f2&itok=7wCBtOIO", "price":400000, "moreInfo": "Rất rất ít calory"},
+        {"id":4, "category":4,"name":"Pizza","img":"https://www.johnniewalker.com/media/5696/wg_drinkwhisky_c63_r_d.jpg?mode=pad&amp;width=768&amp;rnd=132489869960000000", "price":100000, "moreInfo": "Nhiều calory"},
+        {"id":5, "category":4,"name":"Pizza","img":"https://www.johnniewalker.com/media/5696/wg_drinkwhisky_c63_r_d.jpg?mode=pad&amp;width=768&amp;rnd=132489869960000000", "price":200000, "moreInfo": "Có cồn, uống nhiều say, bị phạt"},
+        {"id":6, "category":1,"name":"Pizza","img":"https://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg", "price":600000, "moreInfo": "Nhiều calory"},
+        {"id":7, "category":4,"name":"Pizza","img":"https://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg", "price":90000, "moreInfo": "Nhiều calory"},
+        {"id":8, "category":4,"name":"Pizza","img":"https://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg", "price":90000, "moreInfo": "Nhiều calory"},
+        {"id":9, "category":3,"name":"Pizza","img":"https://www.efsa.europa.eu/sites/default/files/styles/share_opengraph/public/2021-03/experts_food_additives.jpg?h=827069f2&itok=7wCBtOIO", "price":400000, "moreInfo": "Rất rất ít calory"}    
+      ],
+
+    "Category":[
+      {"id":1, "name":"Món chính"},
+      {"id":2, "name":"Tráng miệng"},
+      {"id":3, "name":"Khai vị"},
+      {"id":4, "name":"Đồ uống"}
+      
+
+    ]
+}
